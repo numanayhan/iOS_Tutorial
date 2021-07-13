e85175957ff853e12520007d05a2f3ae08538444
mba <>
2021-07-14 00:21:41 +0300

    create app

---
+++ iOS_Tutorial/iOS_Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Tutorial
+//
+//  Created by Melike Büşra Ayhan on 14.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
